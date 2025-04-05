5c1d7da7aa57ab2612fcf0c9a7cb0cc26a56379c
nischal1101 <kaflenk123@gmail.com>
2025-04-05 12:38:16 +0545

    add loading indicator

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,5 +1,9 @@
-const loading = () => {
-  return <div>loading.....</div>;
+const Loading = () => {
+  return (
+    <div className="h-100vh w-full flex items-center justify-center">
+      <svg className="size-10 animate-spin ..." viewBox="0 0 24 24"></svg>;
+    </div>
+  );
 };
 
-export default loading;
+export default Loading;
